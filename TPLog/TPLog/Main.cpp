f0369bf3f5bdb96b5511b1b8f23b28b41b30528f
SchroterQuentin <quentin.schroter@numericable.fr>
2015-11-23 14:41:28 +0100

    Ajout du projet TPLog

---
+++ TPLog/TPLog/Main.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+TPLog  -  fichier main
+-------------------
+début                : 23/11/2015 14:30:19
+copyright            : (C) 2015 par Quentin SCHROTER, Nicolas GRIPONT
+e-mail               : quentin.schroter@insa-lyon.fr , nicolas.gripont@insa-lyon.fr
+*************************************************************************/
+
+int main()
+{
+	return 0;
+}
