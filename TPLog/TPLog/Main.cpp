fc9144680323135889186b610b2da6ff82d9a2e4
SchroterQuentin <quentin.schroter@numericable.fr>
2015-11-23 15:44:50 +0100

    Include Main

--- TPLog/TPLog/Main.cpp
+++ TPLog/TPLog/Main.cpp
@@ -5,6 +5,8 @@ début                : 23/11/2015 14:30:19
 copyright            : (C) 2015 par Quentin SCHROTER, Nicolas GRIPONT
 e-mail               : quentin.schroter@insa-lyon.fr , nicolas.gripont@insa-lyon.fr
 *************************************************************************/
+#include "Utilisateur.h"
+#include "Document.h"
 
 int main()
 {
